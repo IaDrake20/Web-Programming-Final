fb3a0245d5b6f7031a2dc2b3fbeeba17197a00d6
IaDrake20 <ios.drake05@gmail.com>
2023-04-03 18:25:02 -0400

    started on random access for db

--- backendserver.js
+++ backendserver.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const path = require("path");
+const sqlite3 = require('sqlite3');
+
+//path to database
+const path_lootGen = '/db/loot_generator';
 
 const app = express();
 const PORT = 3001;
+
+const db_loot = sqlite3.Database(path_lootGen, (err) =>
+    if(err) {
+      console.error(err.message);
+    }
+    console.log('Connected to loot generator database');
+)
 
 // temp
 app.get("/", (req, res) => {
