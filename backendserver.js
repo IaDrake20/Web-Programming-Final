10df642e8aa10749ea4f3839f5230592f3222e71
IaDrake20 <ios.drake05@gmail.com>
2023-04-05 13:44:46 -0400

    pulling sqlite out of backend to put in mongodb

--- backendserver.js
+++ backendserver.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const path = require("path");
-const sqlite3 = require("sqlite3");
 
 //path to database
 const path_lootGen = "/db/loot_generator";
 
 const app = express();
 const PORT = 3001;
-
-const db_loot = new sqlite3.Database(path_lootGen, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log("Connected to loot generator database");
-});
 
 //db_loot.get('SELECT * FROM Items ORDER BY RANDOM')
 
