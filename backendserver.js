ee0f954bd7fc092f00c767cba7f3e6d53478cd72
Hunter Kalinoski <HMK30@pitt.edu>
2023-04-05 13:12:25 -0400

    fixed sqlite3 error

--- backendserver.js
+++ backendserver.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const path = require("path");
-const sqlite3 = require('sqlite3');
+const sqlite3 = require("sqlite3");
 
 //path to database
-const path_lootGen = '/db/loot_generator';
+const path_lootGen = "/db/loot_generator";
 
 const app = express();
 const PORT = 3001;
 
-const db_loot = sqlite3.Database(path_lootGen, (err) => {
-    if(err) {
-      console.error(err.message);
-    }
-    console.log('Connected to loot generator database');
-  });
+const db_loot = new sqlite3.Database(path_lootGen, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to loot generator database");
+});
 
 //db_loot.get('SELECT * FROM Items ORDER BY RANDOM')
 
@@ -23,8 +23,8 @@ app.get("/", (req, res) => {
 });
 
 const item = {
-    Name: 'default',
-    Value: '0'
+  Name: "default",
+  Value: "0",
 };
 
 app.listen(PORT, () => {
