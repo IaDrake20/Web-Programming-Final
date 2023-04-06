009f9750fb7c1b044c8d1b16ebf4d74c0e51624b
Hunter Kalinoski <HMK30@pitt.edu>
2023-04-06 00:56:30 -0400

    Added UI bar images and functionality

--- client/script.js
+++ client/script.js
@@ -6,11 +6,11 @@ let hp_max = 100;
 let mana_current = 10;
 let mana_max = 10;
 
-let action_points_current = 4;
-let action_points_max = 4;
+let ap_current = 4;
+let ap_max = 4;
 
-let experience_current = 0;
-let experience_threshold = 25;
+let xp_current = 0;
+let xp_threshold = 25;
 
 let character_level_current = 1;
 let character_level_max = 10;
@@ -25,51 +25,100 @@ let player_stats = {
   hp_current,
   mana_current,
   mana_max,
-  action_points_current,
-  action_points_max,
-  experience_current,
-  experience_threshold,
+  ap_current,
+  ap_max,
+  xp_current,
+  xp_threshold,
   character_level_current,
   character_level_max,
 };
+const healthValues = document.querySelector("#health-values");
+const manaValues = document.querySelector("#mana-values");
+const actionPointsValues = document.querySelector("#action-points-values");
+const experienceValues = document.querySelector("#experience-values");
 
-// TODO: querySelect the actual div/p/whatever elements
-// TODO: set background of these to a semi-filled in bar that represents the ratio of first : second value.
-const healthDiv = document.querySelector("#health");
-healthDiv.innerText = `Health ${player_stats.hp_current}/${player_stats.hp_max}`;
+const healthBar = document.querySelector("#health-bar");
+const manaBar = document.querySelector("#mana-bar");
+const actionPointsBar = document.querySelector("#action-points-bar");
+const experienceBar = document.querySelector("#experience-bar");
 
-const manaDiv = document.querySelector("#mana");
-manaDiv.innerText = `Mana ${player_stats.mana_current}/${player_stats.mana_max}`;
+const updatePlayerStats = () => {
+  healthValues.innerText = `${player_stats.hp_current}/${player_stats.hp_max}`;
 
-const actionPointsDiv = document.querySelector("#action-points");
-actionPointsDiv.innerText = `AP ${player_stats.action_points_current}/${player_stats.action_points_max}`;
+  manaValues.innerText = `${player_stats.mana_current}/${player_stats.mana_max}`;
 
-const experienceDiv = document.querySelector("#experience");
-experienceDiv.innerText = `XP ${player_stats.experience_current}/${player_stats.experience_threshold}`;
+  actionPointsValues.innerText = `${player_stats.ap_current}/${player_stats.ap_max}`;
+
+  experienceValues.innerText = `${player_stats.xp_current}/${player_stats.xp_threshold}`;
+
+  let hpPercentage = player_stats.hp_current / player_stats.hp_max;
+  let manaPercentage = player_stats.mana_current / player_stats.mana_max;
+  let apPercentage = player_stats.ap_current / player_stats.ap_max;
+  let xpPercentage = player_stats.xp_current / player_stats.xp_threshold;
+
+  healthBar.style.width = `${150 * hpPercentage}px`;
+  manaBar.style.width = `${150 * manaPercentage}px`;
+  actionPointsBar.style.width = `${150 * apPercentage}px`;
+  experienceBar.style.width = `${150 * xpPercentage}px`;
+};
+updatePlayerStats();
 
 const middleDiv = document.querySelector("#middle");
-const actionInput = document.querySelector("#action-input");
-actionInput.addEventListener("keypress", (event) => {
-  // on pressing enter, put message in middleDiv
-  if (event.key === "Enter") {
-    event.preventDefault();
-    const p = document.createElement("p");
-    p.innerText = actionInput.value;
-    middleDiv.appendChild(p);
 
-    // scroll middleDiv down to see the new entry
-    p.scrollIntoView();
+const handleInput = (event) => {
+  if (!actionInput.value) return;
+  event.preventDefault();
+  const p = document.createElement("p");
+  const value = actionInput.value;
+  p.innerText = value;
 
-    // clear value of middle div
-    actionInput.value = "";
+  // scroll middleDiv down to see the new entry
+  p.scrollIntoView();
+
+  // clear value of middle div
+  actionInput.value = "";
+
+  // if user inputs a keyword, subtract (or add) to player stats
+  // temporary, for testing
+  if (value === "hp") {
+    player_stats.hp_current -= 5;
+    p.style.color = "red";
+    if (player_stats.hp_current <= 0) player_stats.hp_current = 0;
+  } else if (value === "mana") {
+    player_stats.mana_current -= 1;
+    p.style.color = "blue";
+    if (player_stats.mana_current <= 0) player_stats.mana_current = 0;
+  } else if (value === "mana +") {
+    player_stats.mana_current += 1;
+    p.style.color = "blue";
+    if (player_stats.mana_current >= 10) player_stats.mana_current = 10;
+  } else if (value === "ap") {
+    player_stats.ap_current -= 1;
+    p.style.color = "green";
+    if (player_stats.ap_current <= 0) player_stats.ap_current = 0;
+  } else if (value === "xp") {
+    player_stats.xp_current += 5;
+    p.style.color = "yellow";
+    if (player_stats.xp_current >= player_stats.xp_threshold)
+      player_stats.xp_current = player_stats.xp_threshold;
   }
+  updatePlayerStats();
+
+  middleDiv.appendChild(p);
+};
+
+const actionInput = document.querySelector("#action-input");
+actionInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") handleInput(e);
 });
 
-const levelDiv = document.querySelector("#characterLevel");
-levelDiv.innerText = `Level: ${player_stats.character_level_current}/${player_stats.character_level_max}`;
+document.querySelector("#submit-button").addEventListener("click", (e) => handleInput(e));
 
-const chaos = document.querySelector("#chaosLevel");
-chaos.innerText = `${chaos_level_current}/${chaos_level_max}`;
+const levelDiv = document.querySelector("#player-level");
+levelDiv.innerText = `Level: ${player_stats.character_level_current}`;
+
+const chaos = document.querySelector("#chaos-level");
+chaos.innerText = `${chaos_level_current}`;
 
 let scoreDiv = document.querySelector("#score");
 scoreDiv.innerText = score;
--- clientserver.js
+++ clientserver.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "index.html"));
