c6e408d2975517f483c53a9776923587d7fcfa07
IaDrake20 <ios.drake05@gmail.com>
2023-04-06 16:56:41 -0400

    set up a basic connection to the db

--- backendserver.js
+++ backendserver.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const path = require("path");
-
-//path to database
-const path_lootGen = "/db/loot_generator";
-
 const app = express();
 const PORT = 3001;
+const uri = "mongodb+srv://B_group:<P4qSHiG7vKUkq5bn>@cluster0.zpcyqgd.mongodb.net/test"; //need safer way to store password, maybe enviornment or something
 
-//db_loot.get('SELECT * FROM Items ORDER BY RANDOM')
+//setup connection to mongodb
+const { MongoClient} = require('mongodb');
+
+async function main(){
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    await db_items(client);
+  } catch(e){
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+main.catch(console.error);
+}
+
+
+
+
 
 // temp
 app.get("/", (req, res) => {
