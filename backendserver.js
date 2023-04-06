78885df0fa664a00f3500df8e86b7250651e0ba1
Hunter Kalinoski <HMK30@pitt.edu>
2023-04-06 18:55:17 -0400

    Mongodb connection

--- backendserver.js
+++ backendserver.js
@@ -1,32 +1,47 @@
-//login details
-const user = process.env.USERNAME;
-const pswrd = process.env.PASSWORD;
-
 const express = require("express");
 const path = require("path");
 const app = express();
+
+// import dotenv (.env)
+require("dotenv").config();
+//login details
+const user = process.env.DB_USER;
+const pswrd = process.env.DB_PASS;
+
 const PORT = 3001;
-const uri = "mongodb+srv://${user}:<${pswrd}>@cluster0.zpcyqgd.mongodb.net/test"; //need safer way to store password, maybe enviornment or something
+const uri = `mongodb+srv://${user}:${pswrd}@cluster0.zpcyqgd.mongodb.net/test`; //need safer way to store password, maybe enviornment or something
 
 //setup connection to mongodb
-const { MongoClient} = require('mongodb');
+const { MongoClient } = require("mongodb");
 
-async function main(){
+async function main() {
   const client = new MongoClient(uri);
   try {
-    await client.connect();
-    await db_items(client);
-  } catch(e){
+    const mongo = await client.connect();
+    console.log("Connected to mongo!");
+    const db = mongo.db("db_loot");
+
+    // db_loot
+    const myCollection = db.collection("db_loot");
+
+    // add to database
+    myCollection.insertOne({
+      item: "canvas",
+      qty: 100,
+      tags: ["cotton"],
+      size: { h: 28, w: 35.5, uom: "cm" },
+    });
+
+    // get results
+    let results = await myCollection.find({}).limit(50).toArray();
+    console.log(results);
+  } catch (e) {
     console.error(e);
   } finally {
     await client.close();
   }
-main.catch(console.error);
 }
-
-
-
-
+main().catch(console.error);
 
 // temp
 app.get("/", (req, res) => {
