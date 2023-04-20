b0e07e80b681eca5ef5e2a50c02f31b1696a5052
MoeF159 <ofarag115@gmail.com>
2023-04-20 09:13:30 -0400

    CORS issue resolved

--- backendserver.js
+++ backendserver.js
@@ -228,7 +228,7 @@ app.use(express.json());
 
 // fix CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
